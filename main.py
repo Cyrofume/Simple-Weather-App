0ae8942840dd9e76ffb8c5098536e623321b658a
unknown <angelpg@vt.edu>
2024-08-23 21:14:47 -0400

    centered GUI, handles cloud weather only

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 ##modules
-
+import os
+from dotenv import load_dotenv, dotenv_values
 import sys #handles system variables
 import PyQt5.Qt
 import PyQt5.QtWidgets
 import requests #make request to API's
 import PyQt5
 # from PyQt5.QtWidgets import QApplication, QWidget ,QLabel, QlineEdit, QPushButton, QVBoxLayout #Front-end
-
+#qt core
+from PyQt5.QtCore import Qt
 ##functions
 print("weather API")
+load_dotenv()
+# weather_api_key = os.getenv("WK_API")
+weather_api_key = os.getenv("MY_API_WK")
+print(weather_api_key, "api key comes here")
+api_key = weather_api_key
+
 
 window = None
 
@@ -24,8 +32,8 @@ class WeatherWindow(PyQt5.QtWidgets.QWidget):
         self.current_city_input = PyQt5.QtWidgets.QLineEdit(self)
         #these two will overlap, need a widget manger
         self.confirm_city_button = PyQt5.QtWidgets.QPushButton("Confirm Weather", self)
-        self.temperature_label = PyQt5.QtWidgets.QLabel("70*F", self)
-        self.emoji_label = PyQt5.QtWidgets.QLabel("*", self)
+        self.temperature_label = PyQt5.QtWidgets.QLabel("70°F", self)
+        self.emoji_label = PyQt5.QtWidgets.QLabel(self)
         self.description_label = PyQt5.QtWidgets.QLabel("Sunny", self)
         self.setUI()
         # self.setGeometry(0, 0, 1000, 1000)
@@ -47,6 +55,13 @@ class WeatherWindow(PyQt5.QtWidgets.QWidget):
         #set the layout
         self.setLayout(vertBox)
 
+        self.current_city_label.setAlignment(Qt.AlignCenter)
+        self.current_city_input.setAlignment(Qt.AlignCenter)
+        # self.confirm_city_button.setAlignment(Qt.AlignCenter) #has no center element
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
         #align the layout by adding id
         self.current_city_label.setObjectName("currentCityLabel")
         self.current_city_input.setObjectName("currentCityInput")
@@ -54,6 +69,9 @@ class WeatherWindow(PyQt5.QtWidgets.QWidget):
         self.temperature_label.setObjectName("temperatureLabel")
         self.emoji_label.setObjectName("emojiLabel")
         self.description_label.setObjectName("descriptionLabel")
+
+        
+
         #set css stylesheet
 
         #note any mistake will revert back to default
@@ -66,11 +84,87 @@ class WeatherWindow(PyQt5.QtWidgets.QWidget):
                            font-style:bold;
                            font-weight: bold;
                 }
+            QLineEdit#currentCityInput{
+                           font-size:35px;
+                           font-style:bold;
+                   
                            
+                           }
+            QPushButton#confirmCityButton{
+                           font-size:40px;
+                           font-style:bold;
+                           background-color:light gray;
+                           }
+                           
+            QLabel#temperatureLabel{
+                           font-size:100px;
+                           
+                           }
+            QLabel#emojiLabel{
+                           font-size:200px;
+                           padding:100px;
+                           font-family: Noto Emoji;
+                        
+                           }
+            QLabel#descriptionLabel{
+                           font-size:100px;
+                           }
 
             """)
+            
 
         #note id uses # and classes uses .
+    
+        self.confirm_city_button.clicked.connect(self.getWeather)
+    def getWeather(self):
+        print("weather get")
+        #retrieve api key
+        key = api_key
+        #the input we want to get the text written when user clicks on confirm city button
+        city = self.current_city_input.text()
+        #using api to get our response given as JSON
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
+        response = requests.get(url)
+        #convert to JSON object
+        data = response.json() 
+        # print(data)
+        #since its in a dict we can retrieve by getting the key to the value
+        # print(data["cod"])
+        
+        if data["cod"] == 200:
+            #passed and will get all data
+            self.handleError(str(data["cod"]))
+            self.handleWeather(data)
+        else:
+            #this json will contain COD(int) and a message(str)
+            self.handleError((data["message"]))
+        # self.handleError(str(data["cod"]))
+        #data pass and error codes can appear such as 404 or 400, etc
+        
+        pass
+    ##get the temperature_label
+    #add a new text notifying
+    #if city was found or errors found
+    def handleError(self, errorMessage):
+        self.temperature_label.setText(errorMessage)
+        self.errorCode = errorMessage
+        # pass
+
+    # self.temperature_label = PyQt5.QtWidgets.QLabel("70°F", self)
+    # self.emoji_label = PyQt5.QtWidgets.QLabel("☀️", self)
+    # self.description_label
+    def handleWeather(self, data):
+        # if self.errorCode == 200:
+        # self.temperature_label.setText(data["weather"]["main"])
+        weatherData = data["weather"]
+        weatherData[0]
+        print(weatherData[0]["main"])
+        self.emoji_label.setText(weatherData[0]["main"])
+        currentWeather = weatherData[0]["main"]
+
+        if currentWeather == "Clouds":
+            self.emoji_label.setText("⛅")
+        # pass
 
     
     # pass
--- readme.txt
+++ readme.txt
@@ -1,9 +1,14 @@
 In order to run the Weather APP
 
+-
 The following modules need to be downloaded
 
+pip install python-dotenv
 pip install requests
-
 pip install PyQt
+
+-
+get env WK_API
+
 
 keyterms: Qt, PyQt5, requests
